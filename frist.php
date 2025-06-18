61865df5fc01b1bc10afafc7e9a1739d0ae0b3cf
REHAN <jhrrrs760@gmail.com>
2025-06-18 13:22:40 -0700

    Add files via upload

---
+++ frist.php
@@ -0,0 +1,21 @@
+<?php 
+ echo "hello word<br>";
+ print "using print : hello word<br>";
+ $var = 10 ;
+ $var2 = 20 ;
+ $var3 = $var + $var2 ;
+ echo "$var3<br>" ;
+
+ $string = "hello word";
+ $float = 10.2;
+ $boolean = true ;
+ 
+ echo "string variable :$string<br>" ;
+ echo "float value :$float<br>" ;
+ echo "boolean value :$boolean<br>" ;
+ #single line comment
+//single line comment
+/* multi line commment */
+?> 
+
+
